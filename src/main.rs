f018dd6b02331035f1ef69202d363ec0a9f14485
Thane Thomson <connect@thanethomson.com>
2020-07-16 17:41:52 -0400

    Make hello world testable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+fn say_hello(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", say_hello("world"));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_say_hello() {
+        assert_eq!("Hello world!", say_hello("world"));
+    }
 }
